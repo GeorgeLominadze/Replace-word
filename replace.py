d362490a3acebd693b3411da71ad0947bef72a48
elvislifts <113297342+elvislifts@users.noreply.github.com>
2023-01-22 16:42:12 +0400

    initial commit

---
+++ replace.py
@@ -0,0 +1,7 @@
+def replace_word():
+    str = 'Hello my name is George and i am a python developer'
+    word_to_replace = input('Enter word to replace:')
+    word_replacement = input('Enter word replacement:')
+    print(str.replace(word_to_replace,word_replacement))
+
+replace_word()
