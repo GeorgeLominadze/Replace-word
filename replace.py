a00a62c01d7b76a576164173185494d9150fd326
elvislifts <113297342+elvislifts@users.noreply.github.com>
2023-01-22 18:03:03 +0400

    Update replace.py

--- replace.py
+++ replace.py
@@ -1,5 +1,5 @@
 def replace_word():
-    str = 'Hello my name is George and i am a python developer'
+    str = 'Hello my name is George and i am a python programmer'
     word_to_replace = input('Enter word to replace:')
     word_replacement = input('Enter word replacement:')
     print(str.replace(word_to_replace,word_replacement))
